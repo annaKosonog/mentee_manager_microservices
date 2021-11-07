e22b8ac731e36a25179f38010af07035ae2ac5d3
annaKosonog <anna.kosonog21@gmail.com>
2021-11-07 17:28:05 +0100

    "Create initial Spring boot API"

---
+++ src/test/java/pl/akademia/logging_application/LoggingApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.akademia.logging_application;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LoggingApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
