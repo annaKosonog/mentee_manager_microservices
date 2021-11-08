f5a5a946f66809ce401fc0e4f56f12ad57f19ffa
annaKosonog <anna.kosonog21@gmail.com>
2021-11-08 16:32:15 +0100

    "Setting up Swagger"

---
+++ src/main/java/pl/akademia/logging_application/config/SwaggerConfig.java
@@ -0,0 +1,24 @@
+package pl.akademia.logging_application.config;
+
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+@Configuration
+@EnableSwagger2
+@ConditionalOnProperty(value = "swagger.enabled", matchIfMissing = true)
+public class SwaggerConfig {
+
+    @Bean
+    public Docket docket() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .select()
+                .paths(PathSelectors.regex("^(?!/(error).*$).*$"))
+                .paths(PathSelectors.any())
+                .build();
+    }
+}
