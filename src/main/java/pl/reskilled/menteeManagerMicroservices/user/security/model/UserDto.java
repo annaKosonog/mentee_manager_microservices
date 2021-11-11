2c9c826765d35585380e4db6a147af4a0eb0c33c
annaKosonog <anna.kosonog21@gmail.com>
2021-11-11 21:05:46 +0100

    "Add domain class UserDto"

---
+++ src/main/java/pl/reskilled/menteeManagerMicroservices/user/security/model/UserDto.java
@@ -0,0 +1,11 @@
+package pl.reskilled.menteeManagerMicroservices.user.security.model;
+
+import java.time.Duration;
+
+public class UserDto {
+    private String name;
+    private String email;
+    private  String futurePosition;
+    private Duration time;
+
+}
