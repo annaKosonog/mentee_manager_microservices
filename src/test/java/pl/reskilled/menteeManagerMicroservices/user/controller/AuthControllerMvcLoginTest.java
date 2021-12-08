2f31ca7460a9866c8aae32ff2dd3ac67ef433ae4
annaKosonog <anna.kosonog21@gmail.com>
2021-12-08 10:38:19 +0100

    "Import class SampleLoginDto  "

--- src/test/java/pl/reskilled/menteeManagerMicroservices/user/controller/AuthControllerMvcLoginTest.java
+++ src/test/java/pl/reskilled/menteeManagerMicroservices/user/controller/AuthControllerMvcLoginTest.java
@@ -1,4 +1,4 @@
-package pl.reskilled.menteeManagerMicroservices.user.controller;
+package java.pl.reskilled.menteeManagerMicroservices.user.controller;
 
 
 import com.fasterxml.jackson.databind.ObjectMapper;
@@ -14,9 +14,9 @@ import org.testcontainers.containers.MongoDBContainer;
 import org.testcontainers.junit.jupiter.Container;
 import org.testcontainers.junit.jupiter.Testcontainers;
 import pl.reskilled.menteeManagerMicroservices.MenteeManagerMicroservices;
-import pl.reskilled.menteeManagerMicroservices.user.model.SampleLoginDto;
 import pl.reskilled.menteeManagerMicroservices.user.security.model.LoginDto;
 
+import java.pl.reskilled.menteeManagerMicroservices.user.model.SampleLoginDto;
 import java.pl.reskilled.menteeManagerMicroservices.user.model.SampleUser;
 
 import static org.assertj.core.api.Assertions.assertThat;
