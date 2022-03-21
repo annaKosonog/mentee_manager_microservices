3cb9d1fb492e3ff614a345a6d2422736493ddc09
annaKosonog <anna.kosonog21@gmail.com>
2022-03-21 17:34:50 +0100

    Fixed method to download csv file for project and mentee

--- src/main/java/pl/reskilled/menteeManagerMicroservices/project/csv/CsvProjectController.java
+++ src/main/java/pl/reskilled/menteeManagerMicroservices/project/csv/CsvProjectController.java
@@ -3,91 +3,33 @@ package pl.reskilled.menteeManagerMicroservices.project.csv;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
-import org.supercsv.io.CsvBeanWriter;
-import org.supercsv.io.ICsvBeanWriter;
-import org.supercsv.prefs.CsvPreference;
-import pl.reskilled.menteeManagerMicroservices.mentee.domain.MenteeService;
-import pl.reskilled.menteeManagerMicroservices.mentee.domain.dto.MenteeDto;
-import pl.reskilled.menteeManagerMicroservices.project.domain.ProjectService;
-import pl.reskilled.menteeManagerMicroservices.project.domain.dto.ProjectDto;
 
 import javax.servlet.http.HttpServletResponse;
+import java.io.File;
 import java.io.IOException;
-import java.io.OutputStreamWriter;
-import java.io.Writer;
-import java.nio.charset.StandardCharsets;
-import java.text.DateFormat;
-import java.text.SimpleDateFormat;
-import java.util.Date;
-import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/api")
 public class CsvProjectController {
 
-    private final ProjectService projectService;
-    private final MenteeService menteeService;
+    private final CsvView csvView;
 
 
     @GetMapping("/csv/project")
-    public void returningCsvContentFromAnApiProject(HttpServletResponse response) throws IOException {
+    public void returningCsvContentFromAnApiProject(HttpServletResponse response, @RequestParam("file") File fileName) throws Exception {
 
-        response.setContentType("text/csv");
-        DateFormat dateFormatter = new SimpleDateFormat("yyyy-MM-dd_HH-mm-ss");
-        String currentDateTime = dateFormatter.format(new Date());
+        csvView.prepareResponse(response, fileName);
+        csvView.buildCsvDocumentProject(response);
 
-        String headerKey = "Content-Disposition";
-        String headerValue = "attachment; filename=project_" + currentDateTime + ".csv";
-        response.setHeader(headerKey, headerValue);
-
-
-        List<ProjectDto> listProject = projectService.findAllProject();
-
-        Writer writer = new OutputStreamWriter(response.getOutputStream(), StandardCharsets.UTF_8.toString());
-        ICsvBeanWriter csvWriter = new CsvBeanWriter(writer, CsvPreference.STANDARD_PREFERENCE);
-
-
-        String[] csvHeader = {"Name", "Developers", "Tech stack", "Description"};
-        String[] nameMapping = {"name", "developers", "techStack", "description"};
-
-        csvWriter.writeHeader(csvHeader);
-
-        for (ProjectDto dto : listProject) {
-            csvWriter.write(dto, nameMapping);
-
-        }
-        csvWriter.close();
     }
 
-
     @GetMapping("/csv/mentees")
-    public void returningCsvContentFromAnApiMentees(HttpServletResponse response) throws IOException {
-        response.setContentType("text/csv");
-        DateFormat dateFormatter = new SimpleDateFormat("yyyy-MM-dd_HH-mm-ss");
-        String currentDateTime = dateFormatter.format(new Date());
+    public void returningCsvContentFromAnApiMentees(HttpServletResponse response, @RequestParam("file") File fileName) throws IOException {
 
-        String headerKey = "Content-Disposition";
-        String headerValue = "attachment; filename=mentees_" + currentDateTime + ".csv";
-        response.setHeader(headerKey, headerValue);
-
-
-        List<MenteeDto> listProject = menteeService.getAllStudents();
-
-        Writer writer = new OutputStreamWriter(response.getOutputStream(), StandardCharsets.UTF_8.toString());
-        ICsvBeanWriter csvWriter = new CsvBeanWriter(writer, CsvPreference.STANDARD_PREFERENCE);
-
-
-        String[] csvHeader = {"Username", "Email", "Future_position", "Duration", "Seniority"};
-        String[] nameMapping = {"username", "email", "future_position", "duration", "seniority"};
-
-        csvWriter.writeHeader(csvHeader);
-
-        for (MenteeDto dto : listProject) {
-            csvWriter.write(dto, nameMapping);
-
-        }
-        csvWriter.close();
+        csvView.prepareResponse(response, fileName);
+        csvView.buildCsvDocumentMentee(response);
     }
 }
---
+++ src/main/java/pl/reskilled/menteeManagerMicroservices/project/csv/CsvView.java
@@ -0,0 +1,77 @@
+package pl.reskilled.menteeManagerMicroservices.project.csv;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Component;
+import org.supercsv.io.CsvBeanWriter;
+import org.supercsv.io.ICsvBeanWriter;
+import org.supercsv.prefs.CsvPreference;
+import pl.reskilled.menteeManagerMicroservices.mentee.domain.MenteeService;
+import pl.reskilled.menteeManagerMicroservices.mentee.domain.dto.MenteeDto;
+import pl.reskilled.menteeManagerMicroservices.project.domain.ProjectService;
+import pl.reskilled.menteeManagerMicroservices.project.domain.dto.ProjectDto;
+
+import javax.servlet.http.HttpServletResponse;
+import java.io.File;
+import java.io.IOException;
+import java.io.OutputStreamWriter;
+import java.io.Writer;
+import java.nio.charset.StandardCharsets;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.List;
+
+@Component
+@RequiredArgsConstructor
+public class CsvView {
+
+    private final ProjectService projectService;
+    private final MenteeService menteeService;
+
+    protected void prepareResponse(HttpServletResponse response, File fileName) {
+        response.setContentType("text/csv");
+        DateFormat dateFormatter = new SimpleDateFormat("yyyy-MM-dd_HH-mm-ss");
+        String currentDateTime = dateFormatter.format(new Date());
+
+        String headerKey = "Content-Disposition";
+        String headerValue = "attachment; filename=" + fileName +"_" + currentDateTime + ".csv";
+
+        response.setHeader(headerKey, headerValue);
+    }
+
+    protected void buildCsvDocumentProject(HttpServletResponse response) throws IOException {
+
+        Writer writer = new OutputStreamWriter(response.getOutputStream(), StandardCharsets.UTF_8);
+        writer.write('\uFEFF');
+        ICsvBeanWriter csvWriter = new CsvBeanWriter(writer, CsvPreference.EXCEL_NORTH_EUROPE_PREFERENCE);
+
+        List<ProjectDto> listProject = projectService.findAllProject();
+
+        String[] csvHeader = {"Name", "Developers", "Tech stack", "Description"};
+        String[] nameMapping = {"name", "developers", "techStack", "description"};
+        csvWriter.writeHeader(csvHeader);
+
+        for (ProjectDto dto : listProject) {
+            csvWriter.write(dto, nameMapping);
+        }
+            csvWriter.close();
+    }
+
+    protected void buildCsvDocumentMentee(HttpServletResponse response) throws IOException {
+        Writer writer = new OutputStreamWriter(response.getOutputStream(), StandardCharsets.UTF_8.toString());
+        ICsvBeanWriter csvWriter = new CsvBeanWriter(writer, CsvPreference.EXCEL_NORTH_EUROPE_PREFERENCE);
+
+        List<MenteeDto> listMentee = menteeService.getAllStudents();
+
+        String[] csvHeader = {"Username", "Email", "Future_position", "Duration", "Seniority"};
+        String[] nameMapping = {"username", "email", "future_position", "duration", "seniority"};
+
+        csvWriter.writeHeader(csvHeader);
+
+        for (MenteeDto dto : listMentee) {
+            csvWriter.write(dto, nameMapping);
+
+        }
+        csvWriter.close();
+    }
+    }
